4ce720bee51a871cad3595f9c9e5d48156fc3838
刘凯 <61678390@qq.com>
2020-05-22 17:00:19 +0800

    初始化

---
+++ mall-ums/src/main/java/com/mine/mall/ums/MallUmsApplication.java
@@ -0,0 +1,13 @@
+package com.mine.mall.ums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallUmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallUmsApplication.class, args);
+    }
+
+}
